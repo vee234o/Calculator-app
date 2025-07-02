50667c3124d2c67cec82dda7d1e5a3112ec36da7
Vee234o <vee.orlas@email.com>
2025-07-02 13:35:38 +0100

    1st

---
+++ script.js
@@ -0,0 +1,20 @@
+function appendValue(value) {
+  if (value === '^') {
+    document.getElementById('display').value += '**';
+  } else {
+    document.getElementById('display').value += value;
+  }
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
+
+function calculate() {
+  try {
+    let result = eval(document.getElementById('display').value);
+    document.getElementById('display').value = result;
+  } catch {
+    document.getElementById('display').value = 'Error';
+  }
+}
